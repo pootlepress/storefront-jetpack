ae98177334f53240937536aa8a5beef414236a81
Shramee <shramee.srivastav@gmail.com>
2015-06-03 20:17:20 +0530

    Addressing #2
    WPPB init

---
+++ includes/class-sfjp-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       http://pootlepress.co.uk
+ * @since      1.0.0
+ *
+ * @package    Sfjp
+ * @subpackage Sfjp/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Sfjp
+ * @subpackage Sfjp/includes
+ * @author     PootlePress <nick@pootlepress.co.uk>
+ */
+class Sfjp_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
